2b4ac3d28c2665edf42b19476d1a932b22c2ad8a
Sean Amadio <sean.amadio@gmail.com>
2015-06-11 17:22:39 -0400

    Inital Project Commit

---
+++ TapBPM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapBPM
+//
+//  Created by Sean Amadio on 2015-06-11.
+//  Copyright (c) 2015 Boltmade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
