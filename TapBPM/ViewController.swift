f2404a7d49b2215e6a19930be98a12ea98692287
Sean Amadio <sean.amadio@gmail.com>
2015-06-12 14:06:33 -0400

    Added a calculation for bpm and printing it to the console

--- TapBPM/ViewController.swift
+++ TapBPM/ViewController.swift
@@ -9,9 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Create a place to store each of our bpms so we can get an average later
+    private var samples:[Double] = [];
+    
+    //We declare a private var that stores a previous date
+    private var previousDate:NSDate?;
 
     @IBAction func didTap(recognizer:UIGestureRecognizer) {
-        println("Tapped!");
+        
+        let now = NSDate();
+        //Check to see if we have a previous date stored
+        if let date = previousDate
+        {
+            let bpm = 60/now.timeIntervalSinceDate(date);
+            samples += [bpm];
+            println(bpm);
+        }
+        else
+        {
+            
+        }
+        previousDate = now;
     }
 }
 
