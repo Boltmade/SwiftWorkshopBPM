8e0250fedd5e40069e0626af78d10f9a4451876b
Michelle Ellis <michelle@boltmade.com>
2015-06-25 12:12:15 -0400

    Begin keeping BPM samples

--- TapBPM/TapBPM/ViewController.swift
+++ TapBPM/TapBPM/ViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private var samples:[Int] = []
+    private var lastTapTime:NSDate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func didTap(sender: UITapGestureRecognizer) {
-        println("tapped")
+        collectBPMSample(NSDate(), optionalLastSample: self.lastTapTime)
     }
 
+    private func collectBPMSample(now : NSDate, optionalLastSample : NSDate?) {
+        if let lastSample = optionalLastSample {
+            let bpm = Int(60/now.timeIntervalSinceDate(lastSample))
+            self.samples += [bpm]
+            println(bpm)
+        }
+        self.lastTapTime = now
+    }
 }
 
