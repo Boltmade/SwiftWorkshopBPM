3b8acde1ea442c288ef12fb75ca12c66074e68e4
Michelle Ellis <michelle@boltmade.com>
2015-06-25 10:44:13 -0400

    Create project

---
+++ TapBPM/TapBPM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TapBPM
+//
+//  Created by Michelle Ellis on 2015-06-24.
+//  Copyright (c) 2015 Boltmade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TapBPM/TapBPMTests/TapBPMTests.swift
@@ -0,0 +1,36 @@
+//
+//  TapBPMTests.swift
+//  TapBPMTests
+//
+//  Created by Michelle Ellis on 2015-06-24.
+//  Copyright (c) 2015 Boltmade. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TapBPMTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
